2e59513605c21b16fab2b1400329b7475c6d61ab
Tomoya Hayashi <tomoya128@gmail.com>
2023-06-24 11:54:40 +0900

    add create_book mutation

---
+++ app/graphql/input_types/book.rb
@@ -0,0 +1,4 @@
+class InputTypes::Book < Types:BaseInputObject
+  graphql_name 'BookAttributes'
+  argument :title, String, required: true
+end
---
+++ app/graphql/mutations/create_book.rb
@@ -0,0 +1,15 @@
+app/graphql/mutations/create_book.rb
+module Mutations
+  class CreateBook < Mutations::BaseMutation
+    argument :params, InputTypes::Book, required: true
+
+    field :book, ObjectTypes::Book, null: false
+
+    def resolve(params:)
+      book = Book.create!(params.to_h)
+      { book: book }
+    rescue => e
+      GraphQL::ExecutionError.new(e.message)
+    end
+  end
+end
