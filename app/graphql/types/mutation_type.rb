5015b99fe01617a1cd4329493bedab6aa8aa2222
Tomoya Hayashi <tomoya128@gmail.com>
2023-06-25 21:13:42 +0900

    add Mutations::DeleteBook

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_book, mutation: Mutations::CreateBook
     field :update_book, mutation: Mutations::UpdateBook
+    field :delete_book, mutation: Mutations::DeleteBook
   end
 end
